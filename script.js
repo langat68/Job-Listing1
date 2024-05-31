f590221bf33635c1be3e64359d103b359847cbdf
langat68 <langatfidel32gmail.com>
2024-05-31 10:26:05 +0300

    added  for function to fetch data from api

--- script.js
+++ script.js
@@ -9,3 +9,25 @@ const header = document.querySelector('header');
 (async function() {
     let res = await fetch("./data.json");
     let data = await res.json();
+
+    for (let curr of data) {
+        const container = document.createElement('div');
+        container.classList.add('container');
+        container.innerHTML = `<img src="${curr.logo}" alt="logo">
+        <div class="head">
+        <div class="wrapper">
+            <div class="top">
+                <h2>${curr.company}</h2>
+    
+                <div class="stat">
+                </div>
+            </div>
+    
+            <a href="#">${curr.position}</a>
+    
+            <ul class="availability">
+                <li>${curr.postedAt}</li>
+                <li>${curr.contract}</li>
+                <li>${curr.location}</li>
+            </ul>
+        </div>
