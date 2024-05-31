d00b01a27d9c35abf9874a41b7c21ec36c229070
langat68 <langatfidel32gmail.com>
2024-05-31 10:45:02 +0300

    added data from json

--- script.js
+++ script.js
@@ -31,11 +31,13 @@ const header = document.querySelector('header');
                 <li>${curr.location}</li>
             </ul>
         </div>
-        <hr>
+
+          <hr>
           
           <div class="tags">
           </div>`;
-          if (curr.new) {
+
+        if (curr.new) {
             const span = document.createElement('span');
             span.classList.add('new');
             span.textContent = 'New!'
@@ -48,6 +50,7 @@ const header = document.querySelector('header');
         span.textContent = 'Featured'
         container.querySelector('.stat').append(span);
         }
+
         let types = curr.position.split(' ');
 
         const btn1 = document.createElement('button');
@@ -72,3 +75,93 @@ const header = document.querySelector('header');
             btn.textContent = t;
             container.querySelector('.tags').appendChild(btn);
         }
+
+        main.appendChild(container);
+    }
+
+    const buttons = main.querySelectorAll('.tags button');
+    buttons.forEach(button => {
+        button.onclick = () => {
+            filterType(button.dataset.type);
+            updateContainer();
+        }
+    })
+    
+})();
+
+clear.onclick = () => {
+    arr = [];
+    filter.innerHTML = '';
+    main.querySelectorAll('.container').forEach(container => container.classList.remove('remove'));
+    filter.closest('.filter').style.display = 'none';
+}
+
+function addFilter() {
+    filter.innerHTML = '';
+    for (i of arr) {
+        const el = document.createElement('div');
+        el.classList.add('span');
+        el.innerHTML = `<p>${i}</p>
+        <button aria-label="remove button"><img src="images/icon-remove.svg" alt=""></button>`;
+        filter.appendChild(el);
+        filter.closest('.filter').style.display = 'flex';
+
+        // remove filter element
+        closes = [];
+        closes.push(el.querySelector('button'));
+        closes.forEach(close => {
+            close.onclick = () => {
+                filter.removeChild(close.closest('.span'));
+                arr.splice(arr.indexOf(close.previousElementSibling.textContent), 1)
+                updateContainer();
+                if (filter.innerHTML) {
+                    filter.closest('.filter').style.display = 'flex';
+                } else {
+                    filter.closest('.filter').style.display = 'none';
+                }
+            }
+        })
+    }
+}
+
+function filterType(type) {
+    if (!arr.includes(type)) {
+        arr.push(type);
+        addFilter();
+    }
+}
+
+function updateContainer() {
+    const containers = main.querySelectorAll('.container');
+    containers.forEach(container => {
+        const buttons = container.querySelectorAll('button');
+        let check = [];
+        for (i of buttons) {
+            check.push(i.dataset.type)
+        }
+
+        let include = true;
+        for (j of arr) {
+            if (!check.includes(j)) {
+                include = false;
+            }
+        }
+        if (!include) {
+            container.classList.add('remove');
+        } else {
+            container.classList.remove('remove');
+        }
+    })
+}
+
+function changeBg() {
+    if (document.body.clientWidth < 700) {
+        header.style["background-image"] = "url(images/bg-header-mobile.svg)";
+    } else {
+        header.style["background-image"] = "url(images/bg-header-desktop.svg)";
+    }
+}
+
+changeBg();
+
+window.onresize = changeBg;
